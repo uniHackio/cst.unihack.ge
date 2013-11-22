0ce3cacd4588f052b4b97911ad80b005cce8b97f
Safareli <i.safareli@gmail.com>
2013-11-23 02:13:33 +0400

    live-server grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,10 @@
         'connect:livereload',
         'watch'
     ]);
+    grunt.registerTask('live-server', [
+        'connect:livereload',
+        'watch:assets'
+    ]);
     grunt.registerTask('server', [
         'connect:server'
     ]);
